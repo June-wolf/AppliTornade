d1b66615221c5a1936256684594bc5fee2ca562c
June-wolf <junewolf01@gmail.com>
2015-01-01 22:04:28 +0100

    AppliTornade
    Application permettant de se renseigner sur les tornades. [TPE 2014-2015]

---
+++ AppliTornade.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+run = 1
+print(" ------------------------ ")
+print("|Choisissez une saison : |")
+print("|                        |")
+print("|[1] Printemps           |")     #Bloc de choix des saisons
+print("|[2] Eté                 |")
+print("|[3] Automne             |")
+print("|[4] Hivers              |")
+print("|                        |")
+print("|[0] Quitter             |")
+print(" ------------------------ ")
+saison = int(input())	                #L'utilisateur entre le numero de la saison choisie
+print(" ------------------------ ")
+print("|Choisissez un des       |")
+print("|    dégats constatés :  |")
+print("|                        |")
+print("|[1] Maisons rasées      |")
+print("|[2] Pt arbres cassés    |")     #Bloc de choix des conséquences
+print("|[3] Toits arrachés      |")
+print("|[4] Immeubles détruits  |")
+print("|[5] Murs renversés      |")
+print("|[6] Arbres déracinés    |")
+print("|                        |")
+print("|[0] Quitter             |")
+print(" ------------------------ ")
+
+consequence = int(input())              #L'utilisateur entre le numero de la consequence observee  
+
+# NB : Retravailler la gestion des erreurs et du run
+if(saison == 1):    #Si la saison choisie 
+	print("Le printemps, plus précisément le mois de mai, peut être considéré comme la saison des tornades.") 
+	print("En effet, 42% d’entre elles s’y produisent.")
+	print("Au printemps les différences de température sur le front polaire sont maximales ; ")
+	print("ce qui crée une atmosphère instable et favorise donc la formation de puissants orages.")
+elif(saison == 2):
+	print("Les températures chaudes de l'été sont propices à la formation d'orages, qui peuvent donner naissance à des tornades.")
+elif(saison == 3):
+	print("L'automne, saison plus fraiche, est donc moins propice aux tornades.")
+elif(saison == 4):
+	print("L'hivers étant une saison généralement très froide, elle n'est presque jamais propice à la création de tornades.")
+else:
+	run += 0
+	
+# Reflechir a creer une fonction saison et consequences separer, pour une meilleur gestion de l'affichage
+if(consequence == 1):
+	print("Echelle de Fujita = 4 / Vitesse du vent = 267-322 km/h / Type = Dévastateurs")
+ 	print("Maisons bien construites et maisons à charpente légère détruites, voitures soufflées à distance")
+	print("et nombreux objets transformés en projectiles.")
+elif(consequence == 2):
+	print("Echelle de Fujita = 0 / Vitesse du vent = 105-137 km/h / Type = Légers") 
+	print("Quelques morceaux de recouvrement de toit enlevés (tuile, etc.), dommages aux gouttières,")
+	print("cheminées et revêtement de façade, branches cassées, arbres à racines de surface renversés")
+elif(consequence == 3):
+	print("Echelle de Fujita = 2 / Vitesse du vent = 179-218 km/h / Type = Considérables")
+ 	print("Toits soufflés sur des maisons bien construites, maisons à charpente légère déplacées de leurs fondations, ")
+	print("maisons mobiles complètement détruites, gros arbres déracinés, objets légers devenus des projectiles, automobiles soulevées.")
+elif(consequence == 4):
+	print("Echelle de Fujita = 5 / Vitesse du vent = >322 km/h / Type = Incroyables")
+	print("Maisons solides rasées et les débris projetés, objets de la grosseur d'une voiture projetés à plus de 100 mètres,") 
+	print("gratte-ciels avec des dommages structuraux, etc.")
+elif(consequence == 5):
+	print("Echelle de Fujita = 3 / Vitesse du vent = 219-266 kkm/h / Type = Sévères")
+	print("Étages complets de maisons solides détruits, dommages importants aux centres commerciaux et les centres d'affaires,")
+	print("trains renversés, arbres écorcés, camions et grosses voitures déplacés, bâtiments légers complètement soufflés à distance")
+elif(consequence == 6):
+	print("Echelle de Fujita = 1 / Vitesse du vent = 138-178 km/h / Type = Modérés")
+	print("Recouvrement de toit complètement enlevés, maisons mobiles endommagées sévèrement, portes extérieures envolées,")
+	print("fenêtres et autres articles en verre cassés.")
+else:
+	run += 0
